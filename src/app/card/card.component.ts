3841aae79c763da9e14e26c4db598e66563cd41c
SD <s_d_a@mail.ru>
2020-06-11 21:18:49 +0300

    initial commit

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+  //
+  // templateUrl: './card.component.html',
+  // styleUrls: ['./card.component.scss']
+
+@Component({
+  selector: 'app-card',
+  template: `
+  <div>
+    <h1>Card Component</h1>
+  </div>
+  `
+})
+export class CardComponent {
+
+}
